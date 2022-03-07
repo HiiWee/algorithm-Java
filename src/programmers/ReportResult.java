14d27fd14517cbd7c8bf91630f76bd72fa0feef7
HiiWee <wpdlzhf159@gmail.com>
2022-03-07 16:53:06 +0900

    [프로그래머스 LV01] 신규 아이디 추천

--- src/programmers/ReportResult.java
+++ src/programmers/ReportResult.java
@@ -18,65 +18,68 @@ import java.util.*;
 class ReportResult {
     public static int[] solution(String[] id_list, String[] report, int k) {
 
-        int[] answer = new int[id_list.length];
+        int idLength = id_list.length;
+        int reportLength = report.length;
+
+        int[] answer = new int[idLength];
+        // key : 신고인, value : 피 신고인
         Map<String, List> reportMap = new HashMap<>();
+        // key : 피 신고인, value : 신고당한 횟수
         Map<String, Integer> reportCnt = new HashMap<>();
-        Map<String, String> reverseReport = new HashMap<>();
         String reportList;
 
-        for (int i = 0; i < id_list.length; i++) {
+        // 2개의 HashMap의 초기화 작업
+        for (int i = 0; i < idLength; i++) {
             reportMap.put(id_list[i], null);
             reportCnt.put(id_list[i], 0);
-            reverseReport.put(id_list[i], null);
         }
-        for (int i = 0; i < report.length; i++) {
+        for (int i = 0; i < reportLength; i++) {
             // report 1개를 받아옴
             reportList = report[i];
             // 신고인과 피 신고인으로 나눔
-            String[] sepReportList = reportList.split(" ");
+            String[] sepReport = reportList.split(" ");
 
             // 이미 존재하는 list가 없으면 새로 만들고 아니면 기존 리스트에 피 신고자 명단을 추가
-            if (reportMap.get(sepReportList[0]) == null) {
-                List<String> defendant = new ArrayList<>();
-                defendant.add(sepReportList[1]);
-                reportMap.put(sepReportList[0], defendant);
+            if (reportMap.get(sepReport[0]) == null) {
+                List<String> newList = new ArrayList<>();
+                newList.add(sepReport[1]);
+                reportMap.put(sepReport[0], newList);
             } else {
-                List<String> exist = reportMap.get(sepReportList[0]);
+                List<String> existList = reportMap.get(sepReport[0]);
                 // 중복이 있다면 넣지 않음
-                if (!exist.contains(sepReportList[1]))
-                    exist.add(sepReportList[1]);
+                if (!existList.contains(sepReport[1]))
+                    existList.add(sepReport[1]);
             }
         }
-        //System.out.println(reportMap);
+        // System.out.println(reportMap);
         // 반복하면서 각 멤버들의 신고당한 횟수를 저장함
-        for (int i = 0; i < id_list.length; i++) {
+        for (int i = 0; i < idLength; i++) {
             List<String> list = reportMap.get(id_list[i]);
             if (list != null) {
-                for (String name: list) {
-                    int count = reportCnt.get(name);
+                for (String respondent: list) {
+                    int count = reportCnt.get(respondent);
                     count++;
-                    reportCnt.put(name, count);
+                    reportCnt.put(respondent, count);
                 }
             }
         }
-        //System.out.println(reportCnt);
-        for (int i = 0; i < id_list.length; i++) {
+        // System.out.println(reportCnt);
+        for (int i = 0; i < idLength; i++) {
+            //  특정 인물(id_list[i]가 신고인)이 신고당한 횟수가 k 이상이면
             if (reportCnt.get(id_list[i]) >= k) {
-                for (int j = 0; j < id_list.length; j++) {
+                for (int j = 0; j < idLength; j++) {
+                    // 피 신고인의 명단을 처음부터 받아옴
                     List<String> list = reportMap.get(id_list[j]);
-                    if (list == null) {
-                        //System.out.println(list);
-                        continue;
-                    } else {
-                        if(list.contains(id_list[i]))
-                            answer[j]++;
+                    // 만약 정지사유가 되는 특정인물이 신고인(id_list[i])이 신고한 리스트에 들어있다면
+                    // 신고인이 메일을 받아야 하므로 answer에서 해당 인덱스의 정수값 1 증가
+                    if (list != null && list.contains(id_list[i])) {
+                        answer[j]++;
                     }
                 }
             }
         }
         return answer;
     }
-
     public static void main(String[] args) {
         String[][] id_list = {{"muzi", "frodo", "apeach", "neo"}, {"con", "ryan"}};
         String[][] report = {{"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"}, {"ryan con", "ryan con", "ryan con", "ryan con"}};
