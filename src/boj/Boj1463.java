20e088b0370049136925bef9f14b16bc21752cf8
HiiWee <wpdlzhf159@gmail.com>
2022-04-01 03:01:43 +0900

    [백준 1463] 1로 만들기

--- src/boj/Boj1463.java
+++ src/boj/Boj1463.java
@@ -1,9 +1,11 @@
 package boj;
 
 /*
+
  * case 2에서, dp[n] > dp[n / 2] + 1 의 조건식에서,
  * dp[n]에 들어갈만한 정답이 dp[n-1]+1인지 dp[n/2]+1 인지를 비교해서 더 작은 값을 넣어야 하는데,
  * 이때 dp[n] 에 dp[n-1]+1이라는 값이 먼저 들어가 있어야 비교가 가능하기 때문에 case 3을 더 먼저 적어야 한다.
+ *
  * */
 
 import java.io.BufferedReader;
