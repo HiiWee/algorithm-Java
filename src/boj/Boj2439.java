d6fbaa18976e914bc15f7794562c5695f645f264
HiiWee <wpdlzhf159@gmail.com>
2022-04-02 23:25:44 +0900

    [백준 2439] 별찍기 - 2

---
+++ src/boj/Boj2439.java
@@ -0,0 +1,19 @@
+package boj;
+
+import java.util.Scanner;
+class Boj2439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int k = 0; k < n - 1 - i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
