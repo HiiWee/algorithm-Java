f5a6f9bba26e5b1833cb2fe834a5701e027825cc
HiiWee <wpdlzhf159@gmail.com>
2022-04-28 21:54:12 +0900

    [백준 10989] 수 정렬하기 3(카운팅 정렬예제)

---
+++ src/boj/Boj10989.java
@@ -0,0 +1,25 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// Counting Sort
+class Boj10989 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int[] arr = new int[10001];
+
+        for (int i = 0; i < n; i++) {
+            arr[Integer.parseInt(bf.readLine())]++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < 10001; i++) {
+            for (int j = 0; j < arr[i]; j++) {
+                sb.append(i).append("\n");
+            }
+        }
+        System.out.print(sb.toString());
+    }
+}
