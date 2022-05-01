8a0332e19cfb4428f91135f85f194fb83c6f9992
HiiWee <wpdlzhf159@gmail.com>
2022-05-01 23:24:10 +0900

    [백준 10828] 스택

---
+++ src/boj/Boj10828.java
@@ -0,0 +1,48 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] temp = bf.readLine().split(" ");
+            String op = temp[0];
+            switch (op) {
+                case "push":
+                    stack.push(Integer.parseInt(temp[1]));
+                    break;
+                case "pop":
+                    try {
+                        System.out.println(stack.pop());
+                    } catch (EmptyStackException e) {
+                        System.out.println(-1);
+                    }
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    if (stack.empty()) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                    break;
+                case "top":
+                    try {
+                        System.out.println(stack.peek());
+                    } catch (EmptyStackException e) {
+                        System.out.println(-1);
+                    }
+                    break;
+            }
+        }
+    }
+}
