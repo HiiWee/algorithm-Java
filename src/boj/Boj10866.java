55bf99f91f4179428067591cd4d4c1d95f23a69f
HiiWee <wpdlzhf159@gmail.com>
2022-05-12 00:11:38 +0900

    [백준 10866] 덱

--- src/boj/Boj10866.java
+++ src/boj/Boj10866.java
@@ -5,43 +5,53 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-class Boj10845 {
+class Boj10866 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        List<Integer> que = new ArrayList<>();
+        List<Integer> deque = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
             String[] line = br.readLine().split(" ");
             String command = line[0];
-            if (command.equals("push")) {
-                que.add(Integer.parseInt(line[1]));
-            } else if (command.equals("pop")) {
-                if (que.isEmpty()) {
+
+            if (command.equals("push_front")) {
+                deque.add(0, Integer.parseInt(line[1]));
+            } else if (command.equals("push_back")) {
+                deque.add(deque.size(), Integer.parseInt(line[1]));
+            } else if (command.equals("pop_front")) {
+                if (deque.isEmpty()) {
                     System.out.println(-1);
                 } else {
-                    System.out.println(que.get(0));
-                    que.remove(0);
+                    System.out.println(deque.get(0));
+                    deque.remove(0);
+                }
+            } else if (command.equals("pop_back")) {
+                if (deque.isEmpty()) {
+                    System.out.println(-1);
+                } else {
+                    System.out.println(deque.get(deque.size() - 1));
+                    deque.remove(deque.size() - 1);
                 }
             } else if (command.equals("size")) {
-                System.out.println(que.size());
+                System.out.println(deque.size());
             } else if (command.equals("empty")) {
-                if (que.isEmpty()) {
+                if (deque.isEmpty()) {
                     System.out.println(1);
                 } else {
                     System.out.println(0);
                 }
             } else if (command.equals("front")) {
-                if (que.isEmpty()) {
+                if (deque.isEmpty()) {
                     System.out.println(-1);
                 } else {
-                    System.out.println(que.get(0));
+                    System.out.println(deque.get(0));
                 }
             } else if (command.equals("back")) {
-                if (que.isEmpty()) {
+                if (deque.isEmpty()) {
                     System.out.println(-1);
                 } else {
-                    System.out.println(que.get(que.size() - 1));
+                    System.out.println(deque.get(deque.size() - 1));
                 }
             }
         }
