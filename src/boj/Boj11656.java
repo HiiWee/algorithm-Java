fe10f73cea9092ba46b0811cf5b51422fe863fdf
HiiWee <wpdlzhf159@gmail.com>
2022-05-13 01:29:07 +0900

    [백준 11656] 접미사 배열: 클래스명 수정

--- src/boj/Boj11656.java
+++ src/boj/Boj11656.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-class Main {
+class Boj11656 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String word = br.readLine();
