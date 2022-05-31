f2418f2de5bd2bf0e644450cc0665da087e601fe
HiiWee <wpdlzhf159@gmail.com>
2022-05-31 15:48:17 +0900

    [백준 1991] 트리 순회: Node 클래스를 이용한 방법 추가

--- src/boj/Boj1991.java
+++ src/boj/Boj1991.java
@@ -3,10 +3,11 @@ package boj;
 import java.io.*;
 import java.util.*;
 
-class Main {
+class Boj1991_1 {
     static Map<String, List<String>> map;
     static String topRoot;
     static StringBuilder sb;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -73,4 +74,81 @@ class Main {
         print(root);
     }
 
+}
+
+class Boj1991_2 {
+    static class Node {
+        int left;
+        int right;
+
+        public Node(int left, int right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static List<Node>[] list;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
+        StringTokenizer st = null;
+        int n = Integer.parseInt(br.readLine());
+        list = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            list[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String root = st.nextToken();
+            int left = st.nextToken().charAt(0) - 'A';
+            int right = st.nextToken().charAt(0) - 'A';
+            list[root.charAt(0) - 'A'].add(new Node(left, right));
+        }
+        preorder(0);
+        sb.append("\n");
+        inorder(0);
+        sb.append("\n");
+        postorder(0);
+        sb.append("\n");
+        bw.write(sb.toString());
+        bw.flush();
+        bw.flush();
+    }
+
+    public static void print(int root) {
+        sb.append((char) (root + 'A'));
+    }
+
+    public static void preorder(int root) {
+        if (root + 'A' == '.') {
+            return;
+        }
+        Node node = list[root].get(0);
+        print(root);
+        preorder(node.left);
+        preorder(node.right);
+    }
+
+    public static void inorder(int root) {
+        if (root + 'A' == '.') {
+            return;
+        }
+        Node node = list[root].get(0);
+        inorder(node.left);
+        print(root);
+        inorder(node.right);
+    }
+
+    public static void postorder(int root) {
+        if (root + 'A' == '.') {
+            return;
+        }
+        Node node = list[root].get(0);
+        postorder(node.left);
+        postorder(node.right);
+        print(root);
+    }
 }
