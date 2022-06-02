2208402d7ccd7d02394d3eb545d97cd6539c82ff
HiiWee <wpdlzhf159@gmail.com>
2022-06-02 15:10:02 +0900

    [백준 1654] 랜선 자르기: 예시 추가

--- src/boj/Boj1654.java
+++ src/boj/Boj1654.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 // 이분탐색 Lower_bound, Uppder_bound 개념 잘 익혀두자
-class Boj1654 {
+class Boj1654_1 {
     static int[] line;
 
     public static void main(String[] args) throws Exception {
@@ -38,5 +38,45 @@ class Boj1654 {
         bw.write(high + "\n");
         bw.flush();
         bw.close();
+    }
+}
+
+class Boj1654_2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        long max = 0;
+        int[] line = new int[k];
+        for (int i = 0; i < k; i++) {
+            line[i] = Integer.parseInt(br.readLine());
+            max = Math.max(line[i], max);
+        }
+
+        long low = 1;
+        long high = max;
+        long mid;
+        while (low <= high) {
+            mid = (low + high) / 2;
+            System.out.println("LOW: " + low + " " + "HIGH: " + high + " MID: " + mid);
+
+            int count = 0;
+            for (int i = 0; i < k; i++) {
+                count += line[i] / mid;
+            }
+            if (count >= n) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        System.out.println("[RESULT]\nLOW: " + low + " HIGH: " + high);
+
+        bw.write(low - 1 + "\n");
+        bw.flush();
+        bw.close();
+
     }
 }
