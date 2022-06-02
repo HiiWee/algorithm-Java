91b05f634536bfd16e62cb05f1b7200ed2fa2b5a
HiiWee <wpdlzhf159@gmail.com>
2022-06-02 17:39:34 +0900

    [백준 2805] 나무 자르기

---
+++ src/boj/Boj2805.java
@@ -0,0 +1,44 @@
+package boj;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        // 나무의 수
+        int n = Integer.parseInt(st.nextToken());
+        // 필요 나무 길이
+        int m = Integer.parseInt(st.nextToken());
+        long[] trees = new long[n];
+        st = new StringTokenizer(br.readLine());
+        long max = 0;
+        for (int i = 0; i < n; i++) {
+            trees[i] = Long.parseLong(st.nextToken());
+            max = Math.max(trees[i], max);
+        }
+
+        long min = 0;
+        long mid;
+        while (min < max) {
+            mid = (min + max) / 2;
+            long length = 0;
+            for (int i = 0; i < n; i++) {
+                if (trees[i] - mid >= 0) {
+                    length += trees[i] - mid;
+                }
+            }
+
+            if (length < m) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+        bw.write(min + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
