01cf4a16661c779db077a660c244f38c17490f61
HiiWee <wpdlzhf159@gmail.com>
2022-06-04 17:41:47 +0900

    [백준 15552] 빠른 A + B

---
+++ src/boj/Boj15552.java
@@ -0,0 +1,21 @@
+package boj;
+
+import java.io.*;
+import java.util.*;
+
+class Boj15552 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            bw.write(A + B + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
