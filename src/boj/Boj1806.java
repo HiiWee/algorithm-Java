b6ace55024596a21c373a568af4891a18050eeac
HiiWee <wpdlzhf159@gmail.com>
2022-07-01 15:28:42 +0900

    [백준 1806] 부분합: 투 포인터, 클래스명 변경

--- src/boj/Boj1806.java
+++ src/boj/Boj1806.java
@@ -3,7 +3,7 @@ package boj;
 import java.util.*;
 import java.io.*;
 
-class Main {
+class Boj1806 {
     static int minCount = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws Exception {
