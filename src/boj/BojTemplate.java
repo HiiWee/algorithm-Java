cfc67d4b4fe8601d7334ec7c6e5b1939ce35a5f6
HiiWee <wpdlzhf159@gmail.com>
2022-08-06 21:53:03 +0900

    Edit: class name

--- src/boj/BojTemplate.java
+++ src/boj/BojTemplate.java
@@ -3,7 +3,7 @@ package boj;
 import java.util.*;
 import java.io.*;
 
-class Main {
+class Template {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
