0a6d3a36e6e146cb02d3eb8d1a0c4bda341588e8
HiiWee <wpdlzhf159@gmail.com>
2022-08-06 21:42:39 +0900

    Add: Boj template
    반복되는 코드의 중복에서 편리함을 찾고자,
    백준 전용 템플릿을 추가합니다

---
+++ src/boj/BojTemplate.java
@@ -0,0 +1,12 @@
+package boj;
+
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
+}
