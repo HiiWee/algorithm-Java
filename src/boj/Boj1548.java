fd2f7f1164f3f47cca2d6e4840b2e7294ce6e1e0
HiiWee <wpdlzhf159@gmail.com>
2022-08-27 18:54:10 +0900

    [백준 1548] 부분 삼각 수열: 클래스명 수정

--- src/boj/Boj1548.java
+++ src/boj/Boj1548.java
@@ -14,7 +14,7 @@ package boj;
 import java.io.*;
 import java.util.*;
 
-class Main {
+class Boj1548 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
