2c6cc63278a29dad6bff85d7774e075c95d06057
HiiWee <wpdlzhf159@gmail.com>
2022-09-09 22:52:51 +0900

    [백준 15649] N과 M (1)

---
+++ src/boj/Boj15649.java
@@ -0,0 +1,50 @@
+package boj;
+
+/*
+    순열을 구한다.
+*/
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        permutation(new boolean[n + 1], new int[m + 1], n, m, 0);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    public static void permutation(boolean[] visited, int[] out, int n, int r, int depth) {
+        if (depth == r) {
+            append(out);
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                out[depth + 1] = i;
+                permutation(visited, out, n, r, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void append(int[] out) {
+        for (int i = 1; i < out.length; i++) {
+            sb.append(out[i]).append(" ");
+        }
+        sb.append("\n");
+    }
+}
