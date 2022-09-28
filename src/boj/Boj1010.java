ed1f88c6a97e9322c9cec876f707b91653f84d83
HiiWee <wpdlzhf159@gmail.com>
2022-09-29 00:44:51 +0900

    [백준 1010] 다리 놓기: 다른DP 방식 추가

--- src/boj/Boj1010.java
+++ src/boj/Boj1010.java
@@ -35,3 +35,30 @@ class Boj1010 {
         bw.close();
     }
 }
+
+class Boj1010_2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[][] dp = new int[30][30];
+
+        dp[0][0] = 1;
+        for (int i = 1; i <= 29; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= i; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            sb.append(dp[m][n]).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
