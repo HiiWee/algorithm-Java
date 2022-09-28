87eb3a0bface9466250af5baa8b82738ad479d2a
HiiWee <wpdlzhf159@gmail.com>
2022-09-29 00:38:40 +0900

    [백준 2407] 조합: 클래스명 변경

--- src/boj/Boj2407.java
+++ src/boj/Boj2407.java
@@ -11,7 +11,7 @@ import java.io.*;
 import java.math.BigInteger;
 import java.util.*;
 
-class Main {
+class Boj2407 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
