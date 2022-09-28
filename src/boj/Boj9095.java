f90bda58e0779e1c249f43cb2d32a6717b60db85
HiiWee <wpdlzhf159@gmail.com>
2022-09-28 23:37:06 +0900

    [백준 9095] 1, 2, 3 더하기: 복습

--- src/boj/Boj9095.java
+++ src/boj/Boj9095.java
@@ -1,28 +1,37 @@
 package boj;
 
-import java.util.Scanner;
+/*
+    1: 1                                            1개
+    2: 1+1, 2                                       2개
+    3: 1+1+1, 1+2, 2+1, 3                           4개
+    4: 7개
+    5: 13
+    6: 24
+    7: 44
+*/
+
+import java.io.*;
 
 class Boj9095 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int cnt = sc.nextInt();
-        int num;
-        for (int i = 0; i < cnt; i++) {
-            num = sc.nextInt();
-            int[] dp = new int[num + 1];
-            countSeperateSum(dp, num);
-            System.out.println(dp[num]);
-        }
-    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
 
-    public static void countSeperateSum(int[] dp, int num) {
+        int[] dp = new int[12];
         dp[1] = 1;
-        if (num > 1) dp[2] = 2;
-        if (num > 2) {
-            dp[3] = 4;
-            for (int i = 4; i <= num; i++) {
-                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-            }
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i <= 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+            sb.append(dp[Integer.parseInt(br.readLine())]).append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
