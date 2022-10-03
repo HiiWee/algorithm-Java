1e3ce0c559d8b7f2a5d760cde737bfebfed85e2d
HiiWee <wpdlzhf159@gmail.com>
2022-10-03 22:09:28 +0900

    [백준 2579] 계단 오르기: 복습

--- src/boj/Boj2579.java
+++ src/boj/Boj2579.java
@@ -10,7 +10,9 @@ package boj;
 */
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 // Top-Down
 class Boj2759_1 {
@@ -64,5 +66,47 @@ class Boj2579_2 {
             dp[i] = Math.max(dp[i - 3] + stairs[i - 1], dp[i - 2]) + stairs[i];
         }
         System.out.println(dp[N]);
+    }
+}
+
+
+
+// 계단 오르기 복습!
+
+/**
+ * 계단을 오를때 마지막 계단(i계단)을 밟는 경우는 2가지가 존재한다.
+ * 1. i - 1 계단을 밟고 i 계단을 밟는 경우
+ * 2. i - 2 계단을 밟고 i 계단을 밟는 경우
+ *
+ * 1번 방법의 경우 i - 1은 반드시 밟아야 하므로 배열의 값을 직접 더해주되 i - 2를 밟게되면 3칸연속 밟을 수 없는 조건을 위배하므로
+ * i - 3번째까지 계단을 밟았을때 최대의 값을 이용한다. -> dp[i] = arr[i - 1] + dp[i - 3]
+ *
+ * 2번 방법의 경우 i - 2계단을 밟고 i계단을 밟아야 하는데, i - 2계단까지 밟았을떄의 최대값을 이용해야 하므로 dp[i] = dp[i - 2]가 된다.
+ *
+ * 따라서 1, 2번 방법중에서 더 최대의 값을 갖는 경우를 i번쨰 dp에 저장한다.
+ */
+
+class Boj2579_3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        dp[1] = arr[1];
+        if (n > 1) {
+            dp[2] = arr[1] + arr[2];
+        }
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 2], arr[i - 1] + dp[i - 3]) + arr[i];
+        }
+
+        bw.write(dp[n] + "\n");
+        bw.flush();
+        bw.close();
+
     }
 }
