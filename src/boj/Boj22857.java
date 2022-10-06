6f3fba4641bc0fd7424c966aa61e5b579e8ba06e
HiiWee <wpdlzhf159@gmail.com>
2022-10-06 21:19:41 +0900

    [백준 22857] 가장 긴 짝수 연속한 부분 수열 (small): DP 풀이 추가

--- src/boj/Boj22857.java
+++ src/boj/Boj22857.java
@@ -47,3 +47,63 @@ class Boj22857 {
         bw.close();
     }
 }
+
+/*
+    2차원 dp를 선언하여 행은 0 ~ k개의 수를 제거했을때를 길이를 구분하고, 각 열들은 n개의 수에서의 최대 부분수열의 길이를 나타냄
+
+    우선 각 수를 받아와서 해당수가 짝수라면 dp[0][i] = dp[0][i - 1] + 1을 해준다.
+    이렇게 하는 이유는 직전에 방문한 수의 길이가 1이라면 현재길이까지 포함해 2의 길이를 가지기 때문이다.
+
+    이렇게 0개의 수를 제거했다면 1 ~ k개의 수를 제거했을때도 판단해주어야 한다.
+    따라서 또다시 1 ~ k의 반복을 하며 현재 수(i)가 홀수인지 짝수인지에 따라 dp를 구분해 채워준다.
+
+    1. 짝수인 경우
+        i가 짝수라면 현재 수를 빼지 않아도 되므로 빼는 수는 유지하고 현재 이전의 수에서 홀수를 제거해야 한다.
+        이후 이전의 수에서 최대값을 저장한다.
+        따라서 dp[j][i] = dp[j][i - 1] + 1의 수를 저장한다.
+    2. 홀수인 경우
+        i가 홀수라면 현재의 수를 제거해야 짝수 연속 부분수열이 성립할 수 있으므로 제거하고, 이전의 최대값을 저장한다.
+        dp[j][i] = dp[j - 1][i - 1] --> 현재 수가 홀수이므로 1을 더하지 않음
+
+    모든 dp를 채웠다면
+    제거할 수 있는 홀수를 제거했을때의 연속 짝수 부분수열의 길이가 가장 길게 나오므로
+    dp[k][i] (i == 1 ~ n) 에서의 최대값을 선택한다.
+*/
+
+// dp 풀이
+class Boj22857_2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[k + 1][n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if (num % 2 == 0) {
+                dp[0][i] = dp[0][i - 1] + 1;
+            }
+
+            for (int j = 1; j <= k; j++) {
+                if (num % 2 == 0) {
+                    dp[j][i] = dp[j][i - 1] + 1;
+                } else {
+                    dp[j][i] = dp[j - 1][i - 1];
+                }
+            }
+        }
+
+        int max = -1;
+        for (int i = 1; i <= n; i++) {
+            max = Math.max(max, dp[k][i]);
+        }
+
+        bw.write(max + "\n");
+        bw.flush();
+        bw.close();
+    }
+}
