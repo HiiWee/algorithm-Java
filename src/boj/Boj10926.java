e41afc0b9437c7b01f20051b9c7341fbcb4a5b37
HiiWee <wpdlzhf159@gmail.com>
2022-12-24 21:28:28 +0900

    [백준 10926] ??

---
+++ src/boj/Boj10926.java
@@ -0,0 +1,16 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Boj10926 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(br.readLine() + "??!");
+        bw.flush();
+        bw.close();
+    }
+}
