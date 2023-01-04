65a5deb34fb527317afd8507ae608f851a392a2e
HiiWee <wpdlzhf159@gmail.com>
2023-01-04 21:46:37 +0900

    [백준 1330] 두 수 비교하기

---
+++ src/boj/Boj1330.java
@@ -0,0 +1,24 @@
+package boj;
+
+import java.io.*;
+import java.util.*;
+
+class Boj1330 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        if (a > b) {
+            bw.write(">");
+        } else if (a < b) {
+            bw.write("<");
+        } else {
+            bw.write("==");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
