6ebe31badee41d418fcb5a7a91d8ddbb9f5da9e7
HiiWee <wpdlzhf159@gmail.com>
2024-01-26 19:50:01 +0900

    [백준 1904] 01 타일 - DP

---
+++ src/boj/Boj1904.java
@@ -0,0 +1,22 @@
+package boj;
+
+import java.io.*;
+
+class Boj1904 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        bw.write(dp[n] + "");
+        bw.flush();
+        bw.close();
+    }
+}
