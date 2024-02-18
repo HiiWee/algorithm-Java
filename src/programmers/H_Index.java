5e6a4ec3a73d8b32e215975a84945d86d0e7b78d
HiiWee <wpdlzhf159@gmail.com>
2024-02-19 03:40:08 +0900

    [PRGM] H-Index - 정렬

---
+++ src/programmers/H_Index.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.*;
+
+class H_Index {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+
+        for (int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
+            if (h <= citations[i]) {
+                answer = h;
+                break;
+            }
+        }
+        return answer;
+    }
+}
