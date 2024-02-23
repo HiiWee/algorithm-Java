9772c91068e55dc1a5edf02c2780bff28c37c941
HiiWee <wpdlzhf159@gmail.com>
2024-02-23 19:01:33 +0900

    [PRGM] 더 맵게 - 우선순위큐

---
+++ src/programmers/더_맵게.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.*;
+
+class 더_맵게 {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int value : scoville) {
+            pq.offer(value);
+        }
+
+        int answer = 0;
+        while (pq.size() > 1) {
+            if (pq.peek() >= K) {
+                break;
+            }
+            answer++;
+            int value1 = pq.poll();
+            int value2 = pq.poll();
+            int newValue = value1 + value2 * 2;
+            pq.offer(newValue);
+        }
+        if (pq.peek() < K) {
+            return -1;
+        }
+        return answer;
+    }
+}
