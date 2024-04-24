2b0af7ad307e6feba89d93b2a8f81f2de67d54c4
HiiWee <wpdlzhf159@gmail.com>
2024-04-24 15:39:42 +0900

    [백준 16922] 로마 숫자 만들기 - 백트래킹, 중복조합

---
+++ src/boj/Boj16922.java
@@ -0,0 +1,38 @@
+package boj;
+
+import java.io.*;
+import java.util.*;
+
+class Boj16922 {
+
+    static int n;
+    static int[] number = {1, 5, 10, 50};
+    static boolean[] visited = new boolean[1001];
+    static int diffCount;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        n = Integer.parseInt(br.readLine());
+
+        dupCombinations(0, 0, 0);
+
+        bw.write(Integer.toString(diffCount));
+        bw.flush();
+        bw.close();
+    }
+
+    public static void dupCombinations(int count, int sum, int index) {
+        if (count == n) {
+            if (!visited[sum]) {
+                visited[sum] = true;
+                diffCount++;
+            }
+            return;
+        }
+
+        for (int i = index; i < 4; i++) {
+            dupCombinations(count + 1, sum + number[i], i);
+        }
+    }
+}
