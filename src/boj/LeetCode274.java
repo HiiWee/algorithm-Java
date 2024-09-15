a2a98f69dba566d12b3a0f3f5cb6dbec49a0aff6
HiiWee <wpdlzhf159@gmail.com>
2024-09-16 03:23:57 +0900

    [LeetCode 274] H-Index - 정렬, 버킷정렬

---
+++ src/boj/LeetCode274.java
@@ -0,0 +1,33 @@
+package boj;
+
+class LeetCode274 {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] buckets = new int[n + 1];
+
+        for (int citation : citations) {
+            if (citation >= n) {
+                buckets[n]++;
+            } else {
+                buckets[citation]++;
+            }
+        }
+
+        for (int bucket : buckets) {
+            System.out.print(bucket + " ");
+        }
+
+        int answer = 0;
+        int count = 0;
+        for (int i = n; i >= 0; i--) {
+            count += buckets[i];
+
+            if (count >= i) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
