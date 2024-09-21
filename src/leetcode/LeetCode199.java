274a230ef287a048cb360cc7ccc36344f6f959f2
HiiWee <wpdlzhf159@gmail.com>
2024-09-22 02:55:39 +0900

    [LeetCode 199] Binary Tree Right Side View - 트리, 그래프탐색

---
+++ src/leetcode/LeetCode199.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import java.util.*;
+
+class LeetCode199 {
+
+    // Definition for a binary tree node.
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> answer = new ArrayList<>();
+        search(0, answer, root);
+
+        return answer;
+    }
+
+    public void search(int level, List<Integer> values, TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        if (values.size() == level) {
+            values.add(node.val);
+        }
+
+        search(level + 1, values, node.right);
+        search(level + 1, values, node.left);
+    }
+}
