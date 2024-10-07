cddaf36779ce10f163f9b869fd514d75e93b33ef
HiiWee <wpdlzhf159@gmail.com>
2024-10-08 02:47:50 +0900

    [LeetCode 49] Group Anagrams - Hash, Sorting

---
+++ src/leetcode/LeetCode49.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.*;
+
+class LeetCode49 {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagrams = new HashMap<>();
+
+        for (int i = 0; i < strs.length; i++) {
+            char[] splits = strs[i].toCharArray();
+            Arrays.sort(splits);
+            anagrams.computeIfAbsent(String.valueOf(splits), (key) -> new ArrayList<>())
+                    .add(strs[i]);
+        }
+
+        return new ArrayList<>(anagrams.values());
+    }
+}
