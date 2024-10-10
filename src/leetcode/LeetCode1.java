647bc37a948e5574fbaee00e2f95ebab602bd191
HiiWee <wpdlzhf159@gmail.com>
2024-10-10 19:56:48 +0900

    [LeetCode 1] Two Sum - Array, HashTable

---
+++ src/leetcode/LeetCode1.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.*;
+
+class LeetCode1 {
+
+    Map<Integer, Integer> numberToIndex = new HashMap<>();
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] answer = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (numberToIndex.containsKey(target - nums[i])) {
+                answer[0] = numberToIndex.get(target - nums[i]);
+                answer[1] = i;
+                break;
+            }
+            numberToIndex.put(nums[i], i);
+        }
+
+        return answer;
+    }
+}
