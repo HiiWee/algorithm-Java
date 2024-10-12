970c371614263c95a9124502dff6965c68872186
HiiWee <wpdlzhf159@gmail.com>
2024-10-12 18:21:24 +0900

    [LeetCode 283] Move Zeroes - Array, Two Pointer

---
+++ src/leetcode/LeetCode283.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+class LeetCode283 {
+    public void moveZeroes(int[] nums) {
+        int fillIndex = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i == fillIndex) {
+                    fillIndex++;
+                    continue;
+                }
+                nums[fillIndex++] = nums[i];
+                nums[i] = 0;
+            }
+        }
+    }
+}
